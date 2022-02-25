44b883189bc42d0c452c8531da2ab7dcddb36aa3
Ricky Morri <raptorzonzon@gmail.com>
2022-02-24 19:04:41 -0600

    More front-end tweaks.

--- app.R
+++ app.R
@@ -1,8 +1,9 @@
 library(shiny)
+library(shinythemes)
 library(shinydashboard)
 library(ggplot2)
 
-ui <- dashboardPage(skin = "red",
+ui <- dashboardPage(skin = "blue",
   dashboardHeader(title = strong("Fvntvsy Footbvll Vis")),
   dashboardSidebar(
     sidebarMenu(
@@ -13,52 +14,91 @@ ui <- dashboardPage(skin = "red",
   dashboardBody(
     tabItems(
       tabItem(tabName = "dashboard",
-        fluidPage(
-          #titlePanel()
-          #sideBarLayout()
-          #sideBarPanel()
-          #mainPanel()
+        fluidPage(theme = shinytheme("lumen"),
+          # dashboard
+            # examples
+            # fact wizard
+            # image wizard
+          
           selectInput("dataset", label = "Dataset", choices = 1970:2019),
           verbatimTextOutput("summary"),
           tableOutput("table"),
           sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
           "then x times 5 is",
           textOutput("product")
-          #numericInput()
-          #textInput()
-          #passwordInput()
-          #textAreaInput()
-          #actionButton()
-          #actionLink()
-          #plotOutput() click, dblclick, and hover
         )
       ),
       
       tabItem(tabName = "wizard",
               fluidRow(
-                box(title = "Axes", status = "warning", solidHeader = TRUE,
-                    width = 12, collapsible = TRUE,
-                    "monkey"),
-              ),
-              fluidRow(
-                box(title = "Filters", status = "warning", solidHeader = TRUE,
-                    width = 12, collapsible = TRUE,
-                    selectInput("players", label = "Players", choices = c(),
+                tags$style(HTML("
+                  .box.box-solid.box-primary>.box-header {
+                    color:#fff;
+                    background:#666666
+                  }
+                  
+                  .box.box-solid.box-primary{
+                    border-bottom-color:#666666;
+                    border-left-color:#666666;
+                    border-right-color:#666666;
+                    border-top-color:#666666;
+                  }
+                                ")),
+                box(title = span("Plot Options", style = "color:white;"), status = "primary", 
+                    solidHeader = TRUE, width = 6, collapsible = TRUE,
+                    column(6, selectInput("x", label = "X-Axis", choices = c(),
+                                multiple = TRUE)),
+                    column(6, selectInput("y", label = "Y-Axis", choices = c(),
+                                multiple = TRUE)),
+                    # options for plot features (smooth line, se = TRUE, etc.)
+                    actionButton("apply_axes", label = "Apply")
+                ),
+                box(title = span("Filters", style = "color:white;"), status = "primary", 
+                    solidHeader = TRUE, width = 6, collapsible = TRUE,
+                    selectizeInput("players", label = "Players", 
+                                choices = NULL,
                                 multiple = TRUE),
-                    selectInput("position", label = "Positions", choices = c(),
-                                multiple = TRUE),
-                    selectInput("teams", label = "Teams", choices = c(),
-                                multiple = TRUE),
-                    # individual players on team and total team
+   #################### players with same name
+                    column(6, selectInput("position", label = "Positions", 
+                                          choices = unique(all$Pos),
+                                          multiple = TRUE)),
+   #################### players with position '0' -> interpret as multiple/ambigious
+                    column(6, selectInput("teams", label = "Teams", 
+                                          choices = c("ARI Cardinals", "ATL Falcons",
+                                                      "BAL Ravens", "BUF Bills",
+                                                      "CAR Panthers", "CHI Bears",
+                                                      "CIN Bengals", "CLE Browns",
+                                                      "DAL Cowboys", "DEN Broncos",
+                                                      "DET Lions", "GB Packers",
+                                                      "HOU Texans", "IND Colts",
+                                                      "JAX Jaguars", "KC Chiefs",
+                                                      "LV Raiders", "LAR Rams",
+                                                      "LAC Chargers", "MIA Dolphins",
+                                                      "MIN Vikings", "NWE Patriots",
+                                                      "NOL Saints", "NYG Giants",
+                                                      "NYJ Jets", "PHI Eagles",
+                                                      "PIT Steelers", "SF 49ers",
+                                                      "STL Seahawks", "TB Buccaneers",
+                                                      "TEN Titans", "WAS Commanders"),
+                                          multiple = TRUE)),
+   #################### individual players on team and total team
+   #################### teams with different acronyms over time
+                        # Chargers: SDG LAC
+                        # Raiders: OAK RAI
+                        # Rams: RAM LAR
+                        # Patriots: BOS NWE
+                        # Cardinals: ARI CAR
+                        # Multiple: 2TM 3TM 4TM
                     sliderInput("years", label = "Years", 1970, 2019, 
                                 value = c(2000, 2019), animate = TRUE),
-                    submitButton("Apply")
+                    actionButton("apply_filter", label = "Apply")
                 )
               ),
               fluidRow(
-                box(title = "Plots", status = "danger", solidHeader = TRUE,
-                    width = 12, 
-                    plotOutput("wizard_plot")
+                box(title = span("Plot", style = "color:white;"), status = "primary", 
+                    solidHeader = TRUE, width = 12, 
+                    plotOutput("wizard_plot"),
+                 #   actionButton("download", label = "Dowload Plot")
                 )
               )
       )
@@ -67,6 +107,12 @@ ui <- dashboardPage(skin = "red",
 )
 
 server <- function(input, output, session) {
+  output$product <- renderText({
+    input$x * 5
+  })
+  
+  updateSelectizeInput(session, "players", choices = unique(all$Player), server = TRUE)
+  
   # observeEvent(input$year_option,
   #              {
   #                if (year_option() == "Multiple")
@@ -83,10 +129,7 @@ server <- function(input, output, session) {
   # output$table <- renderTable({
   #   dataset()
   # })
-
-  output$product <- renderText({
-    input$x * 5
-  })
+  
 
   #renderPlot() res = 96
 }
--- longterm-data-cleaning.R
+++ longterm-data-cleaning.R
@@ -3,7 +3,7 @@ library(tidyverse)
 # started tracking targets in: 1992
 # started tracking fumbles lost in: 1994
 
-clean <- function(dat, b92 = FALSE){
+clean <- function(dat, year, b92 = FALSE){
   new_dat <- select(dat, -c(...1,
                             PassingYds, 
                             PassingAtt, 
@@ -13,45 +13,48 @@ clean <- function(dat, b92 = FALSE){
   new_dat <- new_dat %>%
     relocate(PassTD = PassingTD, .after = Yds...10) %>%
     relocate(RushTD = RushingTD, .after = Yds...13) %>%
-    { if(b92) 
-        relocate(., RecTD = ReceivingTD, .after = Yds...15) 
-      else 
-        relocate(., RecTD = ReceivingTD, .after = Yds...16) } %>%
     { if(b92)
-         rename(., Team = Tm,
-                Games = G,
-                GmStart = GS,
-                PassCmp = Cmp,
-                PassAtt = Att...9,
-                PassYds = Yds...10,
-                RushAtt = Att...12,
-                RushYds = Yds...13,
-                RecYds = Yds...15,
-                YPR = `Y/R`,
-                FPTS = FantasyPoints)
+        relocate(., RecTD = ReceivingTD, .after = Yds...15) %>%
+        rename(., Team = Tm,
+               Games = G,
+               GmStart = GS,
+               PassCmp = Cmp,
+               PassAtt = Att...9,
+               PassYds = Yds...10,
+               RushAtt = Att...12,
+               RushYds = Yds...13,
+               RecYds = Yds...15,
+               YPR = `Y/R`,
+               FPTS = FantasyPoints) %>%
+        add_column(., Tgt = rep(NA, length(dat$Player)), .after = "RushTD")
       else
-         rename(., Team = Tm,
-                Games = G,
-                GmStart = GS,
-                PassCmp = Cmp,
-                PassAtt = Att...9,
-                PassYds = Yds...10,
-                RushAtt = Att...12,
-                RushYds = Yds...13,
-                RecYds = Yds...16,
-                YPR = `Y/R`,
-                FPTS = FantasyPoints) 
-    }
+        relocate(., RecTD = ReceivingTD, .after = Yds...16) %>%
+        rename(., Team = Tm,
+               Games = G,
+               GmStart = GS,
+               PassCmp = Cmp,
+               PassAtt = Att...9,
+               PassYds = Yds...10,
+               RushAtt = Att...12,
+               RushYds = Yds...13,
+               RecYds = Yds...16,
+               YPR = `Y/R`,
+               FPTS = FantasyPoints) } %>%
+    add_column(Year = rep(year, length(dat$Player)), .after = "Age")
   
   return(new_dat)
 }
 
 clean_and_read <- function(x){
   a <- sub("\\.csv$", "", x)
+  year <- parse_integer(a)
   clean(read_csv(paste0("fantasy football data/long-term data + weekly data/yearly/", x)),
-                    b92 = (parse_integer(a) < 1992))
+                    year = year, b92 = (year < 1992))
 }
 
 files <- list.files("fantasy football data/long-term data + weekly data/yearly", "*.csv")
-yearly <- lapply(files, clean_and_read)
+yearly <- files %>%
+  lapply(clean_and_read)
 
+all <- yearly %>%
+  reduce(add_row)
